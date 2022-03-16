1b2505d25617a5673e232b7f47b33b921aed74e4
pericles001 <periclesadjovi@outlook.fr>
2022-03-16 22:02:43 +0100

    Create header and navbar element

---
+++ src/components/header.js
@@ -0,0 +1,22 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.css';
+import {user} from "../user";
+
+export const NavBar = () => {
+    return (
+        <nav className="navbar navbar-dark bg-dark">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="navigation bar">
+                    <span className="badge badge-pill ">
+                        <h4 className='text-white '>Edvora</h4>
+                    </span>
+                </a>
+                <span className='fw-bold text-white' >
+                        {user.name}
+
+                    <img className='m-2 img-fluid img-rounded' src={user.url} alt='user profile image'/>
+                </span>
+            </div>
+        </nav>
+    )
+}
