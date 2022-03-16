0f2c9ff3baa4dcbdb3cd691ac38dcf9d918dd208
pericles001 <periclesadjovi@outlook.fr>
2022-03-16 16:54:20 +0100

    Setting up project

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, {Component} from "react";
+import 'bootstrap/dist/css/bootstrap.css'
+import {rides} from "./rides";
+import {user} from "./user";
+
+export default class App extends Component {
+    state = {
+        user,
+        rides
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+
+            </React.Fragment>
+        )
+    }
+}
+
