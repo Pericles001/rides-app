31b974523582d257899642b68e13dbe426344b56
pericles001 <periclesadjovi@outlook.fr>
2022-03-17 18:06:45 +0100

    Filter states and cities with setState

--- src/App.js
+++ src/App.js
@@ -3,15 +3,45 @@ import 'bootstrap/dist/css/bootstrap.css'
 import {rides} from "./rides";
 import {user} from "./user";
 import {NavBar} from "./components/header";
-import {Filter} from "./components/filter";
+import {Filter, tabDataCity, tabDataState} from "./components/filter";
+import {Widget} from "./components/widget";
 import Ride from "./components/ride";
 import './App.css'
 
 export default class App extends Component {
-
     state = {
         user,
-        rides
+        rides,
+        state: tabDataState,
+        city: tabDataCity,
+    }
+
+    handleCityChange = (city) => {
+        this.setState({
+            city: city
+        })
+        console.log(city.value)
+    }
+
+
+    handleStateChange = (state) => {
+        this.setState({
+            state: state
+
+        })
+        console.log(state.value)
+    }
+
+
+    filter = () => {
+        const {rides, state, city} = this.state;
+        const filteredRides = rides.filter(ride => {
+            return ride.state === state.value && ride.city === city.value
+        })
+        console.log(filteredRides)
+        this.setState({
+            rides: filteredRides
+        })
     }
 
 
@@ -19,17 +49,21 @@ export default class App extends Component {
         return (
             <React.Fragment>
                 <NavBar/>
+
+                <Widget/>
+
                 <Filter city={this.state.rides.map(
-                    ride=>ride.city
+                    ride => ride.city
                 )} state={this.state.rides.map(
-                    ride=>ride.state
-                )} />
+                    ride => ride.state
+                )} onFilter={this.filter} onStateChange={this.handleStateChange} onCityChange={this.handleCityChange}/>
 
 
                 {
                     this.state.rides.map(
                         (ride, index) => (
-                            <Ride key={ride.id} id={ride.id} originStation={ride.origin_station_code} stationPath={ride.station_path}
+                            <Ride key={ride.id} id={ride.id} originStation={ride.origin_station_code}
+                                  stationPath={ride.station_path}
                                   date={ride.date} distance={ride.destination_station_code - ride.origin_station_code}
                                   src={ride.map_url} cityName={ride.city} stateName={ride.state}/>
                         )
