771e42ac72f5c3fa273885a47965dfc0a15aaa0c
pericles001 <periclesadjovi@outlook.fr>
2022-03-17 01:25:40 +0100

    Add subcomponents

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, {Component} from "react";
 import 'bootstrap/dist/css/bootstrap.css'
 import {rides} from "./rides";
 import {user} from "./user";
+import {NavBar} from "./components/header";
+import Ride from "./components/ride";
+import './App.css'
 
 export default class App extends Component {
     state = {
@@ -9,10 +12,21 @@ export default class App extends Component {
         rides
     }
 
+
+
     render() {
         return (
             <React.Fragment>
-
+                <NavBar/>
+                {
+                    this.state.rides.map(
+                        (ride, index) => (
+                    <Ride id={ride.id} originStation={ride.origin_station_code} stationPath={ride.station_path}
+                    date={ride.date} distance={ride.destination_station_code - ride.origin_station_code}
+                    src={ride.map_url} cityName={ride.city } stateName={ ride.state }/>
+                    )
+                    )
+                }
             </React.Fragment>
         )
     }
