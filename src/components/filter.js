ac986289d3830a4b5f97258f2d129fb4f76bb088
pericles001 <periclesadjovi@outlook.fr>
2022-03-17 15:54:06 +0100

    Update code

--- src/components/filter.js
+++ src/components/filter.js
@@ -1,26 +1,23 @@
 import React from "react";
 import Select from "react-select";
+import {customStyles} from "./styleSelect";
 
-var tabDataCity = []
-var tabDataState = []
+let tabDataCity = [];
+let tabDataState = [];
 
 export class Filter extends React.Component {
 
 
     renderStateFilter() {
-        return this.props.state.map((item, index) => {
-            return (
-                tabDataState.push({value: item, label: item})
-            )
+        return this.props.state.map((item) => {
+            return (tabDataState.push({value: item, label: item}))
         })
     }
 
 
     renderCityFilter() {
-        return this.props.city.map((item, index) => {
-            return (
-                tabDataCity.push({value: item, label: item})
-            )
+        return this.props.city.map((item) => {
+            return (tabDataCity.push({value: item, label: item}))
         })
     }
 
@@ -28,8 +25,7 @@ export class Filter extends React.Component {
     render() {
 
 
-        return (
-            <div className='float-end me-3 mt-3'>
+        return (<div className='float-end me-3 mt-3'>
                 <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="white"
                      className="bi bi-text-indent-right me-2 " viewBox="0 0 16 16">
                     <path
@@ -41,28 +37,33 @@ export class Filter extends React.Component {
                         onClick={this.props.onFilter}>Filter
                 </button>
 
-                <div className='card  text-white border-0' style={{width: '150px', background: '#323131'}}>
+                <div className='card  text-white border-0' style={{width: '130px', background: '#323131'}}>
                     <span className='card-title'></span>
+                    <span className='d-none'>{this.renderStateFilter()} </span>
+                    < span className='d-none'>{this.renderCityFilter()}</span>
+
+                    <Select
+                        styles={customStyles}
+                        className=' col-12  '
+                        classNamePrefix="react-select"
+                        name='City'
+                        options={tabDataCity}
+                        placeholder='City'
+                        onChange={this.props.onTypeFilter}/>
 
 
-                    <div className='row'>
-                        <span className='d-none'>{this.renderCityFilter()} </span>
-                        <Select
-
-                            name='City'
-                            options={tabDataCity}
-                            placeholder='City'
-                            onChange={this.props.onTypeFilter}></Select>
-                    </div>
-                    <div className='row'>
-                        <span className='d-none'>{this.renderStateFilter()} </span>
-                        <Select
-                            name='State'
-                            options={tabDataState}
-                            placeholder='State'
-                            onChange={this.props.onTypeFilter}></Select>
-                    </div>
+                    <Select
+                        styles={customStyles}
+                        className='col-12 mt-3 '
+                        name='State'
+                        options={tabDataState}
+                        placeholder='State'
+                        onChange={this.props.onTypeFilter}/>
                 </div>
+                <div className='row'>
+
+                </div>
+
 
             </div>
 
