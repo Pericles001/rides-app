ed3891b0132dfc50784b84703376fe798a7cc4d2
pericles001 <periclesadjovi@outlook.fr>
2022-03-17 19:21:17 +0100

    Done with incrementing counter

--- src/components/widget.js
+++ src/components/widget.js
@@ -7,26 +7,19 @@ export class Widget extends Component{
      return(
          <div className='mt-4 ms-4'>
              <p>
-                 <a className="fork border-0 border-bottom border-light m-2" style={{color:'white' , textDecoration:'none',  }} data-bs-toggle="collapse" href="#collapseExample" role="button"
-                    aria-expanded="false" aria-controls="collapseExample">
+                 <a className="fork border-0 border-bottom border-light m-2" style={{color:'white' , textDecoration:'none',  }} role="button"
+                   >
                      Nearest rides
                  </a>
-                 <a className="fork border-0 border-bottom border-light m-2" style={{color:'white' , textDecoration:'none'}}  type="button" data-bs-toggle="collapse"
-                         data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
-                     Upcoming rides
+                 <a className="fork border-0 border-bottom border-light m-2" style={{color:'white' , textDecoration:'none'}}  type="button"  onClick={this.props.upcoming}>
+                     Upcoming rides ({this.props.count2})
                  </a>
 
-                 <a className="fork border-0 border-bottom border-light m-2" style={{color:'white' , textDecoration:'none', }}  type="button" data-bs-toggle="collapse"
-                         data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
-                     Past rides
+                 <a className="fork border-0 border-bottom border-light m-2" style={{color:'white' , textDecoration:'none', }}  type="button" onClick={this.props.past}>
+                     Past rides ({this.props.count1})
                  </a>
              </p>
-             <div className="collapse" id="collapseExample">
-                 <div className="card card-body">
-                     Some placeholder content for the collapse component. This panel is hidden by default but revealed when
-                     the user activates the relevant trigger.
-                 </div>
-             </div>
+
          </div>
      )
     }
